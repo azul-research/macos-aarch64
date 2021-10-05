2b782e07ddc0e6ebeb908583e51e61334700ce85
Anton Kozlov <akozlov@azul.com>
2021-10-05 22:55:24 +0300

    Add linux-aarch docker files #1

---
+++ dockerfiles/qemu/debootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# https://github.com/openjdk/jdk/blob/83b2219220266c1365466970d08606fef766c4fa/.github/workflows/submit.yml#L514
+
+SYSROOT=$1
+
+qemu-debootstrap \
+	--arch=arm64 \
+	--verbose \
+	--include=fakeroot,symlinks,build-essential,libx11-dev,libxext-dev,libxrender-dev,libxrandr-dev,libxtst-dev,libxt-dev,libcups2-dev,libfontconfig1-dev,libasound2-dev,libfreetype6-dev,libpng-dev \
+	--resolve-deps \
+	buster \
+	$SYSROOT \
+	http://httpredir.debian.org/debian
